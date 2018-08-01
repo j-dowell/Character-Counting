06f4ef9ea0ee863bdc49cc5254c8586cb7893cdb
James Dowell <james.s.dowell@gmail.com>
2018-08-01 19:33:58 +0000

    added input checker

--- counter.js
+++ counter.js
@@ -8,16 +8,29 @@ var amountOf = function(letter, list) {
   return count;
 };
 
-var countLetters = function(str) {
-  noSpaces = str.split(' ').join('');
-  var result = {};
-  for (var i = 0; i < noSpaces.length; i++) {
-    var character = noSpaces[i];
-    if (!Object.keys(result).includes(noSpaces[i])) {
-      result[(noSpaces[i])] = amountOf(character, noSpaces);
-     }
+var stringChecker = function(str) {
+  if (typeof str !== 'string') {
+    return false;
+  } else {
+    return true;
   }
-  return result;
 };
 
-console.log(countLetters('Lighthouse in the house'));
+var countLetters = function(str) {
+    if (!stringChecker(str)) {
+      return 'Enter a string';
+  } else {
+    noSpaces = str.split(' ').join('');
+    var result = {};
+    for (var i = 0; i < noSpaces.length; i++) {
+      var character = noSpaces[i];
+      if (!Object.keys(result).includes(noSpaces[i])) {
+        result[(noSpaces[i])] = amountOf(character, noSpaces);
+       }
+    }
+    return result;
+  }
+};
+
+
+console.log(countLetters('lighthouse in the house'));
